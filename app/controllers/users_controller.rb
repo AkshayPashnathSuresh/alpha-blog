4a0628cf1685e302c30efb1e79c8c761cea031b9
Ubuntu <ubuntu@ip-172-31-36-107.us-east-2.compute.internal>
2020-08-02 14:47:23 +0000

    Add require same user restrictions to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
     
     before_action :set_user, only: [:show, :edit, :update]
-    
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]
     def new
         @user = User.new
     end
@@ -45,4 +46,12 @@ class UsersController < ApplicationController
     def set_user
         @user = User.find(params[:id])
     end
+    
+    def require_same_user
+        if current_user != @user
+            flash[:notice] = "You can only edit your own account"
+            redirect_to @user
+        end
+    end
+    
 end
